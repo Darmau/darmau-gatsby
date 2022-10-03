4fc61b9f86b0f9406b884e2cf250e088b7453462
李大毛 <liaoqifan@gmail.com>
2022-10-04 02:41:26 +0800

    Add head to verify

--- src/components/Head.js
+++ src/components/Head.js
@@ -15,6 +15,8 @@ const Head = ({ title }) => {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="设计师、开发者李大毛的个人网站" />
+        <meta name="bytedance-verification-code" content="T4zz1O2qU5K/j1KTM25K" />
+        <meta name="sogou_site_verification" content="L0VPQ3Fuj4" />
       </Helmet>
     </>
   )
---
+++ src/components/cover/cover.js
@@ -0,0 +1,37 @@
+import React from "react";
+import * as style from "./index.module.css"
+
+export default function Cover() {
+
+  return (
+    <div className={style.coverContainer}>
+
+      <div className={style.codePart} style={{ whiteSpace: 'pre-wrap' }}>
+
+        <div className={style.codeLine1}><span><span className={style.mtk15}>&lt;</span><span className={style.mtk27}>div</span><span className={style.mtk1}>&nbsp;</span><span className={style.mtk6}>class</span><span className={style.mtk15}>=</span><span className={style.mtk5}>"card"</span><span className={style.mtk15}>&gt;</span></span></div>
+
+        <div className={style.codeLine2}><span className={style.mtk15}>&lt;</span><span className={style.mtk27}>svg</span><span className={style.mtk1}>&nbsp;</span><span className={style.mtk6}>width</span><span className={style.mtk15}>=</span><span className={style.mtk5}>"78"</span><span className={style.mtk1}>&nbsp;</span><span className={style.mtk6}>height</span><span className={style.mtk15}>=</span><span className={style.mtk5}>"78"</span><span className={style.mtk1}>&nbsp;</span><span className={style.mtk6}>viewBox</span><span className={style.mtk15}>=</span><span className={style.mtk5}>"0&nbsp;0&nbsp;78&nbsp;78"</span><span className={style.mtk1}>&nbsp;</span><span className={style.mtk6}>fill</span><span className={style.mtk15}>=</span><span className={style.mtk5}>"none"</span><span className={style.mtk15}>&gt;...</span></div>
+
+        <div className={style.codeLine2}><span><span className={style.mtk15}>&lt;/</span><span className={style.mtk27}>svg</span><span className={style.mtk15}>&gt;</span></span></div>
+
+        <div className={style.codeLine2}><span><span className={style.mtk15}>&lt;</span><span className={style.mtk27}>div</span><span className={style.mtk1}>&nbsp;</span><span className={style.mtk6}>class</span><span className={style.mtk15}>=</span><span className={style.mtk5}>"info"</span><span className={style.mtk15}>&gt;</span></span></div>
+
+        <div className={style.codeLine3}><span><span className={style.mtk15}>&lt;</span><span className={style.mtk27}>h1</span><span className={style.mtk1}>&nbsp;</span><span className={style.mtk6}>class</span><span className={style.mtk15}>=</span><span className={style.mtk5}>"name"</span><span className={style.mtk15}>&gt;</span><span className={style.mtk1}>李大毛没有猫</span><span className={style.mtk15}>&lt;/</span><span className={style.mtk27}>h1</span><span className={style.mtk15}>&gt;</span></span></div>
+
+        <div className={style.codeLine3}><span><span className={style.mtk15}>&lt;</span><span className={style.mtk27}>p</span><span className={style.mtk1}>&nbsp;</span><span className={style.mtk6}>class</span><span className={style.mtk15}>=</span><span className={style.mtk5}>"title"</span><span className={style.mtk15}>&gt;</span><span className={style.mtk1}>Designer&nbsp;|&nbsp;Developer</span><span className={style.mtk15}>&lt;/</span><span className={style.mtk27}>p</span><span className={style.mtk15}>&gt;</span></span></div>
+
+        <div className={style.codeLine3}><span><span className={style.mtk15}>&lt;</span><span className={style.mtk27}>p</span><span className={style.mtk1}>&nbsp;</span><span className={style.mtk6}>class</span><span className={style.mtk15}>=</span><span className={style.mtk5}>"intro"</span><span className={style.mtk15}>&gt;</span><span className={style.mtk1}>I&nbsp;create&nbsp;and&nbsp;build&nbsp;things&nbsp;on&nbsp;screen</span><span className={style.mtk15}>&lt;/</span><span className={style.mtk27}>p</span><span className={style.mtk15}>&gt;</span></span></div>
+
+        <div className={style.codeLine2}><span><span className={style.mtk15}>&lt;/</span><span className={style.mtk27}>div</span><span className={style.mtk15}>&gt;</span></span></div>
+
+        <div className={style.codeLine1}><span><span className={style.mtk15}>&lt;/</span><span className={style.mtk27}>div</span><span className={style.mtk15}>&gt;</span></span></div>
+
+      </div>
+
+      <div>
+        开发中
+      </div>
+
+    </div>
+  )
+}
