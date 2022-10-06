94b2b0440734e18b247031a0a89bc4b67b89419d
李大毛 <liaoqifan@gmail.com>
2022-10-06 13:52:31 +0800

    Add RSS Support

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ const strapiConfig = {
 module.exports = {
   siteMetadata: {
     title: `可可托海没有海`,
-    siteUrl: `https://darmau.design`,
+    siteUrl: `https://darmau.design/`,
     description: "李大毛的个人网站",
   },
   plugins: [
@@ -28,7 +28,7 @@ module.exports = {
         "path": "./src/images/"
       },
       __key: "images"
-    }, 
+    },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
@@ -50,19 +50,83 @@ module.exports = {
         background_color: `#fff`,
         theme_color: `#fff`,
         display: `standalone`,
-        icon: `./src/images/favicon.svg`, 
+        icon: `./src/images/favicon.svg`,
       },
     },
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         trackingIds: [
-        process.env.GA_ID, 
+          process.env.GA_ID,
         ],
         pluginConfig: {
           head: true
         },
       },
     },
+
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allStrapiArticle } }) => {
+              return allStrapiArticle.edges.map(edge => {
+                
+                //将正文数据里的文本提取出来
+                const mainContentObject = JSON.parse(edge.node.mainBody.data.mainBody)
+                const blocksObj = mainContentObject.blocks
+                let textArr = ['查看完整内容建议访问原链接。']
+                for (let i in blocksObj) {
+                  textArr.push(blocksObj[i].data.text ? blocksObj[i].data.text : '该文本不支持RSS预览')
+                }
+                const content = textArr.join('<br />')
+
+                return Object.assign({}, edge.node, {
+                  description: edge.node.basic.description,
+                  date: edge.node.basic.date,
+                  url: site.siteMetadata.siteUrl + edge.node.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.slug,
+                  custom_elements: [{ "content:encoded": content }],
+                })
+              })
+            },
+            query: `
+              {allStrapiArticle(sort: {fields: basic___date, order: DESC}) {
+                edges {
+                  node {
+                    basic {
+                      title
+                      description
+                      date
+                    }
+                    slug
+                    mainBody {
+                      data {
+                        mainBody
+                      }
+                    }
+                  }
+                }
+              }}`,
+            output: "/rss.xml",
+            title: "可可托海没有海的RSS",
+          },
+        ],
+      },
+    },
+
   ]
 };
