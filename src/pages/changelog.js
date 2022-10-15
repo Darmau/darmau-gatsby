fc6517d52ea51f403788540110a1c1eeeef09101
李大毛 <liaoqifan@gmail.com>
2022-10-15 22:17:06 +0800

    增加关于本站

--- src/pages/changelog.js
+++ src/pages/changelog.js
@@ -1,11 +1,37 @@
 import React from "react";
-import { Link } from "gatsby";
+import { graphql, Link } from "gatsby";
+import Layout from "../components/layout/layout";
+import MainBody from "../components/TextTransfer"
+import * as style from "../styles/changelog.module.css"
 
-export default function Changelog() {
+export default function Changelog({ data }) {
+  const changeLog = data.allStrapiChangelog.nodes[0].about.data.about
+  return (
+    <Layout>
+      <main className={style.mainContainer}>
+        <h1 className={style.pageTitle}>关于本站</h1>
+        <MainBody data={changeLog}/>
+      </main>
+    </Layout>
+  )
+}
+
+export const changelogQuery = graphql`
+  {
+    allStrapiChangelog {
+      nodes {
+        about {
+          data {
+            about
+          }}}}}`
+
+export function Head() {
   return (
     <>
-      <p>本页面还在开发中</p>
-      <p><Link to="/">返回首页</Link></p>
+      <title>关于本站 | 可可托海没有海</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="设计师、开发者李大毛的个人网站" />
     </>
   )
 }
