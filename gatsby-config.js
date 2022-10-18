b03dcf5288001272c1bc671af853f3b42e66b947
李大毛 <liaoqifan@gmail.com>
2022-10-19 03:17:20 +0800

    remove search

--- gatsby-config.js
+++ gatsby-config.js
@@ -124,15 +124,6 @@ module.exports = {
     },
 
     {
-      resolve: `gatsby-plugin-algolia`,
-      options: {
-        appId: process.env.GATSBY_ALGOLIA_APP_ID,
-        apiKey: process.env.ALGOLIA_ADMIN_KEY,
-        queries: require("./src/utils/algolia-queries")
-      },
-    },
-
-    {
       resolve: `gatsby-omni-font-loader`,
       options: {
         enableListener: true,
