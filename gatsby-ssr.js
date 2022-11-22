08553c5169dd6b8e50c5ba269cbab8d118040c04
李大毛 <liaoqifan@gmail.com>
2022-11-22 22:01:01 +0800

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,9 +1,3 @@
-const React = require('react')
-
-const HtmlAttributes = {
-  lang: "zh"
-}
-
 exports.onRenderBody = ({ setHtmlAttributes }) => {
-  setHtmlAttributes(HtmlAttributes)
+  setHtmlAttributes({ lang: "zh" })
 }
