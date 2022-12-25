61d4e2ddea034f25c81284fb55a35706d4f24f53
李大毛 <liaoqifan@gmail.com>
2022-12-25 16:21:58 +0800

    UI: small adjustment
    将rss、公众号显示在文章列表页

--- src/templates/articles/articles.js
+++ src/templates/articles/articles.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { graphql, Link } from "gatsby";
-import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import { GatsbyImage, getImage, StaticImage } from "gatsby-plugin-image";
 import Layout from "../../components/layout/layout";
 import Pagination from "../../components/pagination/pagination";
 import * as style from "./index.module.css"
+import * as about from "../article-content/index.module.css"
 
 const Articles = ({ data }) => {
   const articles = data.allStrapiArticle.nodes
@@ -35,6 +36,23 @@ const Articles = ({ data }) => {
                 <Link to={'/tag/' + tag.slug}>#{tag.title}</Link>
               </li>))}
             </ul>
+          </div>
+          <div className={about.wechatInfo}>
+            <StaticImage className={about.wechatImg} src="../../images/qrcode.png" />
+            <small className={about.wechatText}>获取本站更新，发表评论，请关注公众号</small>
+            <div className={about.rss}>
+              <svg width="32" height="24" viewBox="0 0 32 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_230_1851)">
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M12 0C18.6275 0 24.0001 5.37257 24.0001 12C24.0001 18.6275 18.6275 24.0001 12 24.0001C5.37257 24.0001 0 18.6275 0 12C0 5.37257 5.37257 0 12 0ZM7.84578 14.6417C7.0141 14.6417 6.33824 15.3198 6.33824 16.1463C6.33824 16.977 7.01403 17.648 7.84578 17.648C8.68004 17.648 9.3554 16.977 9.3554 16.1463C9.3554 15.3198 8.68019 14.6417 7.84578 14.6417ZM6.33968 10.185V12.3556C7.75318 12.3556 9.08151 12.9084 10.082 13.9098C11.0816 14.9083 11.6335 16.243 11.6335 17.662H13.8134C13.8134 13.5397 10.4592 10.1856 6.33968 10.1856V10.185ZM6.34241 6.33795V8.50904C11.3829 8.50904 15.4849 12.6153 15.4849 17.6614L17.6618 17.6616C17.6618 11.4194 12.583 6.33817 6.34256 6.33817L6.34241 6.33795Z" fill="#F26522" />
+                </g>
+                <defs>
+                  <clipPath id="clip0_230_1851">
+                    <rect width="32" height="24" fill="white" />
+                  </clipPath>
+                </defs>
+              </svg>
+              <small className={about.wechatText}><Link to="/rss.xml">RSS</Link></small>
+            </div>
           </div>
         </aside>
         <main className={style.articleContainer}>
