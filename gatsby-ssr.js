059f70fa9748a25efd00b454cf3087e70734aaab
李大毛 <dev@darmau.design>
2023-01-05 09:40:38 +0800

    remove adsense

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -6,12 +6,6 @@ const HtmlAttributes = {
 
 const HeadComponents = [
   <script
-    key="adsense"
-    async
-    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2439295902943960"
-    crossOrigin="anonymous"
-  ></script>,
-  <script
     key="plausible"
     defer
     data-domain="darmau.design"
